aa8c86da554154ed772082e56ba78bfbee43f5ca
soluke22 <soluke22@gmail.com>
2023-01-31 18:14:35 -0600

    Added playermovement

---
+++ MerlinRun/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+	    Vector3 right = transform.right * horizontal * speed * Time.deltaTime;
+
+        Vector3 forward = transform.forward * speed * Time.deltaTime;
+        
+        cc.Move(forward + right);
+    }
+}
