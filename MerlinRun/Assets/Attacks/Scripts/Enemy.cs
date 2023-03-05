bc63462a2d9e2c81ef0fa8233af9bc3551167c64
Procrascat <khalil.luke@gmail.com>
2023-03-04 20:27:59 -0600

    Fix Issue with Attack Wall not Deactivating

--- MerlinRun/Assets/Attacks/Scripts/Enemy.cs
+++ MerlinRun/Assets/Attacks/Scripts/Enemy.cs
@@ -31,6 +31,7 @@ public class Enemy : MonoBehaviour
         {
             Debug.Log("Enemy Hit!");
             Destroy(gameObject);
+            GameObject.Find("AttackWallHitBox").SetActive(false);
         }
     }
 }
--- MerlinRun/Assets/Enemies/Scripts/Obstacles.cs
+++ MerlinRun/Assets/Enemies/Scripts/Obstacles.cs
@@ -19,11 +19,9 @@ public class Obstacles : MonoBehaviour
 
     void SpawnObstacle()
     {
-        GameObject Enemy = Instantiate(Resources.Load("Obstacle", typeof(GameObject))) as GameObject;
-        Enemy.transform.position = gameObject.transform.GetChild(Random.Range(0, 2)).transform.position;
-        Enemy.transform.position = new Vector3(Enemy.transform.position.x, Enemy.transform.position.y + 0.5f, Enemy.transform.position.z);
-
-        Enemy.gameObject.GetComponent<Enemy>().weakness = (AttackType)Random.Range(1, 4);
-        Debug.Log(Enemy.gameObject.GetComponent<Enemy>().weakness);
+        GameObject Obstacle = Instantiate(Resources.Load("Obstacle", typeof(GameObject))) as GameObject;
+        Obstacle.transform.position = gameObject.transform.GetChild(Random.Range(0, 2)).transform.position;
+        Obstacle.transform.position = new Vector3(Obstacle.transform.position.x, Obstacle.transform.position.y + 0.5f, Obstacle.transform.position.z);
+                
     }
 }
