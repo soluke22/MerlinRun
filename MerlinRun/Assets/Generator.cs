a9d8ff57f66273b3167ec6a2f1455988c01f7cfb
soluke22 <soluke22@gmail.com>
2023-03-04 21:57:31 -0600

    fixing movement

--- MerlinRun/Assets/Generator.cs
+++ MerlinRun/Assets/Generator.cs
@@ -12,7 +12,7 @@ public class Generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        platformStartPoint = transform.position;
+        platformStartPoint = platformShape.transform.position;
     }
 
     // Update is called once per frame
@@ -20,7 +20,7 @@ public class Generator : MonoBehaviour
     {
         if (player.transform.position.z > platformStartPoint.z + 10)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 40);
+            transform.position = new Vector3(platformShape.transform.position.x, platformShape.transform.position.y, platformShape.transform.position.z + 70);
             platformStartPoint = transform.position;
             GameObject oldPlatform = platformShape;
             GameObject newPlatform = Instantiate(platformShape, transform.position, transform.rotation);
